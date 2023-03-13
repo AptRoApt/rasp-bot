bc444753a8a47ab15eb53ba6fdc4c5df80efaa1c
AptRoApt <rostislav07.02.05@mail.ru>
2023-03-13 20:48:14 +0300

    ну и нахрена я это делал

--- scheduleRea.py
+++ scheduleRea.py
@@ -75,95 +75,65 @@ def scaleText(text: str, type:str, size: tuple) -> ImageFont.truetype:
     return ImageFont.truetype(type, fontSize)
 
 
+async def getLesson(session, groupName, date, timeSlot) :
+    """Возвращает словарь с данными урока.
 
+    Args:
+        session (aiohttp.ClientSession()): Активная сессия.
+        groupName (str): Имя группы.
+        date (str): Дата урока.
+        timeSlot (int): Номер урока.
 
-
-class Lesson:
-    def __init__(self):#Номер пары,азвание предмета, вид занятия (лекция/семинар...), первая подгруппа (Преподаватель, Аудитория)
-        self.lessonNum = ""
-        self.name = ""
-        self.type = ""
-        self.groups = []
-
-        timetable = {
-                "1 пара": "(08:30 - 10:00):",
-                "2 пара": "(10:10 - 11:40):",
-                "3 пара": "(11:50 - 13:20):",
-                "4 пара": "(14:00 - 15:30):",
-                "5 пара": "(15:40 - 17:10):",
-                "6 пара": "(17:20 - 18:50):",
-                "7 пара": "(18:55 - 20:25):",
-                "8 пара": "(20:30 - 22:00):"
-        }
-        groupsInfo = ""
-        num = 1
-        for group in self.groups:
-            if len(self.groups) == 2:# если есть деление на подгруппы.
-                groupsInfo += f"{num}-я подгруппа:\n"
-            for item in group:
-                groupsInfo += f"{item}\n"
-            num += 1
-        time = timetable.get(self.lessonNum, "" )
-        return f"{self.lessonNum} {time}\n{self.name}\n{self.type}\n{groupsInfo}\n"
-    
-    async def getLesson(self, groupName, date, timeSlot, session) :
+    Returns:
+        lesson = {
+        "name": "",
+        "type": "",
+        "subgroups": [[ИмяУчителя, аудитория], [ИмяУчителя, аудитория]]
+    }
+    """
+    lesson = {
+        "name": "",
+        "type": "",
+        "subgroups": []
+    }
+    target_url = f"https://rasp.rea.ru/Schedule/GetDetails?selection={groupName.lower()}&date={date}&timeSlot={timeSlot}"
+    pageText = await getPageText(session, target_url)
+    soupDetailsPage = BeautifulSoup(pageText, 'html.parser')
+    try:
+        lesson["name"] = soupDetailsPage.find("h5").text #Тега не будет на странице, если пара пустая.
+    except:
+        return None
+    lesson["type"] = soupDetailsPage.find("strong").text
+    groups = soupDetailsPage.find_all("div", class_="element-info-body")
+    teachers = soupDetailsPage.find_all("a")# Первое - ссылка на занятие.
+    k = 1
+    for group in groups: #Если подгруппа только одна - он возьмёт только её, иначе запишет две.
+        index = group.text.find("корпус") - 2 
+        room = group.text[index:index + 10] + group.text[index + 19:index + 23]
+        lesson["subgroups"].append([teachers[k].text[7:], room])# [7:] - обрезаем "school "
+        k += 1
+    return lesson
+    """ 
+        escape-последовательности - один символ
+        room =
+        8 корпус -\r\n        507
+        \________/\_________/\__/
+            10         9      4
+        \___________________/___/
+                  19      + 4 = 23   
         """
-        Получение данных о паре, принимает название группы, дату и номер пары.
-        """
-        target_url = f"https://rasp.rea.ru/Schedule/GetDetails?selection={groupName.lower()}&date={date}&timeSlot={timeSlot}"
-        pageText = await getPageText(session, target_url)
-        soupDetailsPage = BeautifulSoup(pageText, 'html.parser')
-        try:
-            self.name = soupDetailsPage.find("h5").text #Тега не будет на странице, если пара пустая.
-        except:
-            return self
-        lessonNumIndex = soupDetailsPage.div.text.find("пара")-2
-        self.lessonNum = soupDetailsPage.div.text[lessonNumIndex:lessonNumIndex+6]
-        self.type = soupDetailsPage.find("strong").text
-        groups = soupDetailsPage.find_all("div", class_="element-info-body")
-        teachers = soupDetailsPage.find_all("a")# Первое - ссылка на занятие.
-        k = 1
-        for group in groups: #Если подгруппа только одна - он возьмёт только её, иначе запишет две.
-            index = group.text.find("корпус") - 2 
-            room = group.text[index:index + 10] + group.text[index + 19:index + 23]
-            self.groups.append([teachers[k].text[7:], room])# [7:] - обрезаем "school "
-            k += 1
-        return self
-        """ 
-            escape-последовательности - один символ
-            room =
-            8 корпус -\r\n        507
-            \________/\_________/\__/
-                10         9      4
-            \___________________/___/
-                      19      + 4 = 23   
-            """
 
 
 
-class Day:
-    def __init__(self):#Дата занятия, пары
-        self.date = "Ошибка получения даты."
-        self.lessons = []
-    def __str__(self):
-        if len(self.lessons) == 0:
-            return f"{self.date}\nВ этот день занятий нет.\n\n"
-        else:
-            answer = f"{self.date}\n"
-            for lesson in self.lessons:
-                answer += f"{lesson}" 
-            return answer
-# def getDay(groupName, date, session):
-#         """
-#         Заполняет расписание дня. Принимает название группы, дату.
-#         """
-#     self.date = date
-#     tasks = []
-#     for timeSlot in range (1, 9):
-#         task = asyncio.create_task(Lesson().getLesson(groupName, date, timeSlot, session))
-#         tasks.append(task)
-#     self.lessons = await asyncio.gather(*tasks)
-#     return self
+
+async def getDay(session, groupName, date):
+    tasks = []
+    for timeSlot in range (1, 9):
+        task = asyncio.create_task(getLesson(groupName, date, timeSlot, session))
+        tasks.append(task)
+    lessons = await asyncio.gather(*tasks)
+    for lesson in lessons:
+        pass
 
 
 
@@ -173,7 +143,7 @@ async def getWeek(session, groupName: str, weekNum=-1) -> str:
 
     Args:
         session (aiohttp.ClientSession()): Активная сессия.
-        gtoupName (str): Имя группы.
+        groupName (str): Имя группы.
         weekNum (int): Номер недели (по-умолчанию -1).
 
     Returns:
@@ -219,20 +189,9 @@ async def getWeek(session, groupName: str, weekNum=-1) -> str:
         filepath = f"images/{time.time()}.png"
         image.save(filepath)
         return filepath
-        """
-            На длинных предметах едет вёрстка. При всём, кроме ариала
-            Добавить даты недели
-            Добавить легенду
-            """ 
 
-async def testDay():
-    async with aiohttp.ClientSession() as session:
-        day = Day()
-        start = time.time()
-        await day.getDay("15.27д-пи05/22б", "11.03.2023", session)
-        end = time.time()
-        print (end - start)
-        print (day)
+
+
 
 async def testWeek():
     async with aiohttp.ClientSession() as session:
